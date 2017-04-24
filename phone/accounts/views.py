2329f0d405f8fec0b443b033ade1cda4d397ed03
Jae min Baek <jbaek7023@gmail.com>
2017-04-24 11:42:59 +0900

    User can register

--- phone/accounts/views.py
+++ phone/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .forms import UserCreationForm, UserChangeForm
+
 # Create your views here.
+def register(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        print("user has been created")
+    context = {
+        "form" : form
+    }
+    return render(request, "accounts/register.html", context)
--- phone/phone/urls.py
+++ phone/phone/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from accounts.views import register
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^register/$', register),
 ]
