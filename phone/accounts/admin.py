882d2c277c9ad6251e4d7eb38a647398a1bd93d1
Jae min Baek <jbaek7023@gmail.com>
2017-04-24 11:08:03 +0900

    Create Built-In User app : accounts

---
+++ phone/accounts/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import MyUser
+
+# Reference: Django Documentation
+# https://docs.djangoproject.com/en/1.11/topics/auth/customizing/
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('username', 'email',  'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('zipcode',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_staff',)}),
+        ('Access', {'fields': ('is_active',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('username', 'email',)
+    ordering = ('username', 'email',)
+    filter_horizontal = ()
+
+
+admin.site.register(MyUser, UserAdmin)
+
+
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
